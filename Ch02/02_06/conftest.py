757497e3357980579d1a6766b0f3dec16b418a06
Andrey Makarov <winandfx@gmail.com>
2023-06-06 12:05:28 +0300

    [02_06] parametrize

--- Ch02/02_06/conftest.py
+++ Ch02/02_06/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 import yaml
 
+
 @pytest.fixture(scope="session")
 def cases():
     with open("Ch02/02_06/split_cases.yml") as f:
--- Ch02/02_06/test_splitter_func.py
+++ Ch02/02_06/test_splitter_func.py
@@ -1,7 +1,21 @@
+import pytest
+import yaml
 from splitter import split_to_chunks
+
+
+def param_cases():
+    with open("Ch02/02_06/split_cases.yml") as f:
+        for i in yaml.safe_load(f):
+            yield i["size"], i["chunk_size"], i["chunks"]
+
 
 def test_splitter_func(cases: list):
     for i in cases:
         sz = i["size"]
         ch_sz = i["chunk_size"]
         assert [list(i) for i in split_to_chunks(sz, ch_sz)] == i["chunks"]
+
+
+@pytest.mark.parametrize("size, chunk_size, chunks", param_cases())
+def test_splitter_func2(size, chunk_size, chunks):
+    assert [list(i) for i in split_to_chunks(size, chunk_size)] == chunks
