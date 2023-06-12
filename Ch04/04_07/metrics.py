c303fa8f94574e99ee74b25f73d0165b328e319f
Makarov Andrey <winandfx@gmail.com>
2023-06-12 13:19:42 +0300

    Task 04_07

--- Ch04/04_07/metrics.py
+++ Ch04/04_07/metrics.py
@@ -1,6 +1,14 @@
 import pandas as pd
+import pandera as pa
+
+metrics_scheme = pa.DataFrameSchema({
+    "time": pa.Column(pd.DatetimeTZDtype(tz="UTC")),
+    "metric": pa.Column(str, checks=pa.Check.isin(["mem", "cpu"])),
+    "value": pa.Column(float, checks=pa.Check.greater_than(0)),
+})
 
 
+@pa.check_output(metrics_scheme)
 def load_metrics(jsonl_file):
     return pd.read_json(
         jsonl_file,
